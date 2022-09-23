113966903d4a86ea9143f625cb573deaebb54658
Nick Whitelegg <nickw4426@gmail.com>
2022-09-23 10:49:29 +0100

    Modify example

--- app/src/main/java/com/example/viewmodelstarter/MainActivity.kt
+++ app/src/main/java/com/example/viewmodelstarter/MainActivity.kt
@@ -9,18 +9,22 @@ import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
 
-    private var currentName = ""
+    private var currentSong = Song()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // For info, to show you when onCreate() is called
         AlertDialog.Builder(this).setPositiveButton("OK", null)
-            .setMessage("onCreate()").show()
+            .setMessage("onCreate() called").show()
 
 
         findViewById<Button>(R.id.btnGo).setOnClickListener {
-            currentName = findViewById<EditText>(R.id.etName).text.toString()
-            findViewById<TextView>(R.id.tvName).text = currentName
+            currentSong = Song (
+                findViewById<EditText>(R.id.etTitle).text.toString(),
+                findViewById<EditText>(R.id.etArtist).text.toString()
+            )
+            findViewById<TextView>(R.id.tvName).text = "${currentSong.title} by ${currentSong.artist}"
         }
     }
 }
---
+++ app/src/main/java/com/example/viewmodelstarter/Song.kt
@@ -0,0 +1,3 @@
+package com.example.viewmodelstarter
+
+data class Song (val title: String = "", val artist: String = "")
